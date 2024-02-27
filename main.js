ac086fb6bd0da7e2e2a9f794e4d64e2d7bac2f3b
GANESH-990 <gdhage86@gmail.com>
2024-02-27 11:34:56 +0530

    removed dev tool UI

--- main.js
+++ main.js
@@ -1,91 +1,89 @@
+console.log("mian.js logged...");
 
-
-console.log("mian.js logged...")
-
-const { app, BrowserWindow , screen , ipcMain , clipboard } = require('electron/main')
-const path = require('node:path');
-const url = require('url');
-const ElectronStore = require('electron-store');
+const {
+  app,
+  BrowserWindow,
+  screen,
+  ipcMain,
+  clipboard,
+} = require("electron/main");
+const ElectronStore = require("electron-store");
 ElectronStore.initRenderer();
 
-const screenSize={
-    height:null,
-    width:null,
-    newHeight:0,
-    newWidth:0,
-    x:null
+const screenSize = {
+  height: null,
+  width: null,
+  newHeight: 0,
+  newWidth: 0,
+  x: null,
+};
+
+async function getScreenSize() {
+  const size = screen.getPrimaryDisplay().size;
+  screenSize.height = size.height;
+  screenSize.width = size.width;
+  screenSize.newHeight = Math.floor(size.height * 1);
+  screenSize.newWidth = Math.floor(size.width * 0.2);
+  screenSize.x = screenSize.width - screenSize.newWidth;
 }
 
-async function getScreenSize(){
-     const size =  screen.getPrimaryDisplay().size;
-    screenSize.height = size.height;
-    screenSize.width = size.width;
-    screenSize.newHeight = Math.floor(size.height * 1);
-    screenSize.newWidth = Math.floor(size.width * 0.6);
-    screenSize.x = screenSize.width - screenSize.newWidth;
-}
-
-async function createWindow () {
+async function createWindow() {
   const win = new BrowserWindow({
     width: screenSize.newWidth,
     height: screenSize.height,
-    x:screenSize.x,
-    y:0,
-    show:false,
-    webPreferences:{
-        nodeIntegration:true,
-        contextIsolation:false,
-        enableRemoteModule: true
-    }
-  })
+    x: screenSize.x,
+    y: 0,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
 
-win.loadFile('index.html');
-win.once('ready-to-show',()=>{
-    win.show()
-})
+  win.loadFile("index.html");
+  win.once("ready-to-show", () => {
+    win.show();
+  });
 
   //another meothd
-//   win.loadURL(url.format({
-//     pathname:path.join(__dirname,'index.html'),
-//     protocol:'file',
-//     slashes:true
-//   }));
-
-  
+  //   win.loadURL(url.format({
+  //     pathname:path.join(__dirname,'index.html'),
+  //     protocol:'file',
+  //     slashes:true
+  //   }));
 
   //devtools
-  win.webContents.openDevTools();
-   // Handle the 'toggle-always-on-top' message from the renderer process
-   ipcMain.on('toggle-always-on-top', (event, alwaysOnTop) => {
-    win.setAlwaysOnTop(alwaysOnTop);
-});
+  //win.webContents.openDevTools();
 
+  // Handle the 'toggle-always-on-top' message from the renderer process
+  ipcMain.on("toggle-always-on-top", (event, alwaysOnTop) => {
+    win.setAlwaysOnTop(alwaysOnTop);
+  });
 }
 
-app.whenReady().then( async () => {
-await getScreenSize();
-createWindow();
+app.whenReady().then(async () => {
+  await getScreenSize();
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-       createWindow();
+      createWindow();
     }
-  })
-})
-
-
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-ipcMain.on('copy-to-clipboard', (event, data) => {
-    clipboard.writeText(data);
+  });
 });
 
-ipcMain.on('paste-from-clipboard', (event) => {
-    const data = clipboard.readText();
-    event.reply('clipboard-data', data);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+ipcMain.on("copy-to-clipboard", (event, data) => {
+  clipboard.writeText(data);
+});
+
+ipcMain.on("paste-from-clipboard", (event) => {
+  const data = clipboard.readText();
+  event.reply("clipboard-data", data);
 });
--- renderer.js
+++ renderer.js
@@ -1,17 +1,16 @@
-console.log("renderer 1st line runned..");
-//import { clipboard } from 'electron';
-const { clipboard, nativeImage, ipcRenderer } = require("electron");
-const fs = require('fs');
 
+const { clipboard, nativeImage, ipcRenderer } = require("electron");
 const Store = require("electron-store");
 const store = new Store();
-let data = store.store;
+
+
 
 // to display the list on start
 window.addEventListener("load", () => {
   console.log("windows loadedd...");
   updateList();
 });
+
 
 //update list function
 function updateList() {
@@ -22,12 +21,8 @@ function updateList() {
     list.removeChild(list.firstChild);
   }
   for (const key in data) {
-
-        console.log(` displaying---${key}: ${data[key]}`);
-        createList(key, data[key]);
-
-    
-
+    console.log(` displaying---${key}: ${data[key]}`);
+    createList(key, data[key]);
   }
 }
 
@@ -44,59 +39,51 @@ function createList(key, data) {
     "position-relative"
   );
 
-  if(typeof data === "string"){
+  if (typeof data === "string") {
     store.set(key, data);
     item.appendChild(document.createTextNode(data));
-  }
-  else{
-    store.set(key,data);
+  } else {
+    store.set(key, data);
     item.appendChild(document.createTextNode(data.name));
   }
 
-  item.addEventListener("click",()=>{
-    console.log("button clicked.....",data);
-    if(typeof data === "object"){
-        const path = data.path;
-        if(path.endsWith(".png") || path.endsWith(".jpeg")){
-            const img = nativeImage.createFromPath(path);
-            clipboard.writeImage(img);
-        }
-        else{
-            fs.readFile(path,(err,data)=>{
-                if(err){
-                    console.log("can't read file=",err);
-                    return 0;
-                }
-                clipboard.writeBuffer('application/pdf',data);
-                console.log(" file copied.....")
-            })
-        }
-
-    }
-    else{
-        clipboard.writeText(data);
+  item.addEventListener("click", () => {
+    console.log(typeof data);
+    if (typeof data === "object") {
+      const path = data.path;
+        const img = nativeImage.createFromPath(path);
+        clipboard.writeImage(img);
+        console.log("yoyoyoyo");
+    } else {
+      clipboard.writeText(data);
     }
 
+    let d = document.createElement("div");
+    d.classList.add(
+      "customAlert",
+      "alert",
+      "alert-success",
+      "col-3",
+      "text-center",
+      "m-0",
+      "p-0"
+    );
+    d.textContent = "copied!!!";
 
-       let d =  document.createElement("div");
-    d.classList.add( "customAlert","alert","alert-success","col-3","text-center","m-0","p-0");
-    d.textContent = 'copied!!!';
-
-   item.appendChild(d);
-  setTimeout(() => {
-    item.removeChild(d);
-  }, 500);
-  })
-
+    item.appendChild(d);
+    setTimeout(() => {
+      item.removeChild(d);
+    }, 500);
+  });
 
   //delete button
   let deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("btn")
+  deleteBtn.classList.add("btn");
   //deleteBtn.textContent = "Delete";
   //deleteBtn.appendChild(document.createTextNode("DELETE"));
 
   let icon = document.createElement("i");
-  icon.classList.add("bi","bi-trash","bt");
+  icon.classList.add("bi", "bi-trash", "bt");
   deleteBtn.appendChild(icon);
   deleteBtn.addEventListener("click", (event) => {
     event.stopPropagation();
@@ -127,58 +114,65 @@ const button = document.getElementById("addToListBtn");
 button.addEventListener("click", () => {
   const input = document.getElementById("myInput");
   const currentTime = Date.now();
-  createList(currentTime.toString(), input.value);
-  input.value = "";
-  copyClick();
+  if(input.value !== ""){
+    createList(currentTime.toString(), input.value);
+    input.value = "";
+  }
+
 });
 
 //log all store data
-let btn = document.getElementById("showData");
-btn.addEventListener("click", () => {
-  let data = store.store;
-  console.log("-------stored data-----------");
-  console.log(data);
-  console.log("------------------------------");
-});
-
-
+// let btn = document.getElementById("showData");
+// btn.addEventListener("click", () => {
+//   let data = store.store;
+//   console.log("-------stored data-----------");
+//   console.log(data);
+//   console.log("------------------------------");
+// });
 
 //----------------------------file-------------------------------------------
 
-
 const fileInput = document.getElementById("fileInput");
 
-fileInput.addEventListener("change",(event)=>{
+fileInput.addEventListener("change", (event) => {
+  console.log(event.target.files);
+  if (
+    event.target.files[0].name.endsWith(".png") ||
+    event.target.files[0].name.endsWith(".jpeg")
+  ) {
+    handleDroppedFiles(Date.now().toString(), event.target.files);
+  } else {
+    // console.log("please select correct file format");
+    let alertBox = document.getElementById("alertBox");
+    let alert = document.createElement("div");
+    alert.textContent = "file format not supported";
+    alert.classList.add("alert", "alert-danger", "text-center", "m-0", "p-0");
+    alertBox.appendChild(alert);
 
-    console.log(event.target.files);
-    if(event.target.files[0].name.endsWith(".png") || event.target.files[0].name.endsWith(".jpeg")){
-        handleDroppedFiles( Date.now().toString(),event.target.files);
-    }
-    else{
-        console.log("please select correct file format");
-    }
-    
-})
+    setTimeout(() => {
+      alertBox.removeChild(alert);
+    }, 1000);
+  }
+});
 
-function handleDroppedFiles( key,files) {
-    // Handle the dropped files as needed
-    for (const file of files) {
-        const fileName = file.name;
-        const filePath = file.path;
+function handleDroppedFiles(key, files) {
+  // Handle the dropped files as needed
+  for (const file of files) {
+    const fileName = file.name;
+    const filePath = file.path;
 
-        // Store the file information in Electron Store
-        // store.set(key, {
-        //     name: fileName,
-        //     path: filePath
-        // });
-        createList(key,{
-            name: fileName,
-            path: filePath
-        })
+    // Store the file information in Electron Store
+    // store.set(key, {
+    //     name: fileName,
+    //     path: filePath
+    // });
+    createList(key, {
+      name: fileName,
+      path: filePath,
+    });
 
-        // Add your logic to further process the file as needed
-        // For example, you might want to display or read the file content
-        console.log(`File uploaded: ${fileName}`);
-    
-    }
+    // Add your logic to further process the file as needed
+    // For example, you might want to display or read the file content
+    console.log(`File uploaded: ${fileName}`);
+  }
 }
